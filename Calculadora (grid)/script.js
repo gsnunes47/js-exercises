8848bcbd8f8309c4d479f1912c3f14432eba83d2
Gustavo Nunes <gustavompn06@gmail.com>
2024-03-01 14:59:17 -0300

    Adds visor to calculator

--- Calculadora (grid)/script.js
+++ Calculadora (grid)/script.js
@@ -1,7 +1,26 @@
 let botao = document.querySelectorAll("button")
+let resultado = document.querySelector(".result")
+let string = ''
+let lista = []
+
 
 for(let c = 1; c <= 18; c++){
     botao[c].addEventListener("click", function(){
-        console.log(botao[c].innerText)
+        let number = botao[c].innerText
+        if (number in [1,2,3,4,5,6,7,8,9]){
+            resultado.innerHTML += `${number}`
+            lista.push(number)
+        }
+        else if #operação ou zero{
+
+        }
     })
 }
+
+// let teste = 1
+// if(teste in [1,2,3]){
+//     console.log('true')
+// }
+// else{
+//     console.log('false')
+// }
