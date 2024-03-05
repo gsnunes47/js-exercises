5582a85c6d8bf1ec78e008ff1b9c08f9159968c5
Gustavo Nunes <gustavompn06@gmail.com>
2024-03-05 20:39:13 -0300

    zero and dots are fixed

--- Calculadora (grid)/script.js
+++ Calculadora (grid)/script.js
@@ -10,13 +10,21 @@ let operation
 for(let c = 1; c <= 17; c++){
     botao[c].addEventListener("click", function(){
         let number = botao[c].innerText
+
         if (number == 0){
-            if(lista[0] >= 1){
+            if(lista.length == 0){
+                resultado.innerHTML += number
                 lista.push(number)
-                resultado.innerHTML += `${number}`
             }
-            else{
-                alert('O primeiro número não pode ser 0!')
+            else if (lista[0] != 0){
+                resultado.innerHTML += number
+            }
+        }
+        
+        else if (number == '.'){
+            if (inList(lista, '.') == false){
+                resultado.innerHTML += number
+                lista.push(number)
             }
         }
 
@@ -38,21 +46,36 @@ botao[0].addEventListener("click", function(){
     resultado.innerHTML = ''
     first_number = 0
     second_number = 0
+    lista = []
     operation = ''
 })
 
 botao[18].addEventListener("click", function(){
     second_number = resultado.innerText
     if (operation == '+'){
-        resultado.innerHTML = `${first_number} + ${second_number} = ${parseInt(first_number) + parseInt(second_number)}`
+        resultado.innerHTML = `${first_number} + ${second_number} = ${parseFloat(first_number) + parseFloat(second_number)}`
     }
     else if (operation == '-'){
-        resultado.innerHTML = `${first_number} - ${second_number} = ${parseInt(first_number - second_number)}`
+        resultado.innerHTML = `${first_number} - ${second_number} = ${parseFloat(first_number - second_number)}`
     }
     else if (operation == '*'){
-        resultado.innerHTML = `${first_number} * ${second_number} = ${parseInt(first_number * second_number)}`
+        resultado.innerHTML = `${first_number} * ${second_number} = ${parseFloat(first_number * second_number)}`
     }
     else if (operation == '/'){
-        resultado.innerHTML = `${first_number} / ${second_number} = ${parseInt(first_number / second_number)}`
+        resultado.innerHTML = `${first_number} / ${second_number} = ${parseFloat(first_number / second_number)}`
     }
 })
+
+function inList(list, obj){
+    let ver
+    for(let c = 0; c <= list.length; c++){
+        if(list[c] == obj){
+            ver = true
+            break
+        }
+        else{
+            ver = false
+        }
+    }
+    return ver
+}
